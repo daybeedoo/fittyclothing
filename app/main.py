227be18b4269d6a27765fa2827396c031979b767
Timothy Han <timothyhan@timothys-air.home>
2020-08-18 20:53:20 -0700

    Adding pictures

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
-from flask_login import current_user, login_user, logout_user
+# from flask_login import current_user, loginppi_user, logout_user
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -20,24 +21,6 @@ def contact():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-
-
-
-    # if current_user.is_authenticated:
-    #     return redirect(url_for('home'))
-
-    # form = LoginForm()
-
-    # if form.validate_on_submit():
-    #     user = User()
-    #     login_user(user, remember = form.remember_me.data)
-    #     next_page = request.args.get('next')
-    #     if not next_page or url_parse()
-
-
-
-
-
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
