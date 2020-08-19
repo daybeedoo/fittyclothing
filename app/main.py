10038f4b775200f8c19eec5baf33ca9b4c4a9593
djhan0 <david0han0@gmail.com>
2020-08-18 20:29:10 -0700

    dropdown

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,8 +10,34 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
+
+
+    # if current_user.is_authenticated:
+    #     return redirect(url_for('home'))
+
+    # form = LoginForm()
+
+    # if form.validate_on_submit():
+    #     user = User()
+    #     login_user(user, remember = form.remember_me.data)
+    #     next_page = request.args.get('next')
+    #     if not next_page or url_parse()
+
+
+
+
+
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
